d4d35f929e8d754a4c24a78ba6628938855aa8ed
tomhickey50 <tomhickey50@gmail.com>
2023-12-10 22:24:26 -0600

    Email reset password almost working

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,22 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5b07e16c302697a4'
+app.config['SECRET_KEY'] = b'5b07e16c302697a4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+mail = Mail(app)
 
 from flaskblog import routes

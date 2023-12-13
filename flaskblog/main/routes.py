85dc1876e1d016c3e656a1309c8c7d59ee865528
tomhickey50 <tomhickey50@gmail.com>
2023-12-12 21:09:29 -0600

    Last commit

--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -10,9 +10,7 @@ main = Blueprint('main', __name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    page = request.args.get('page', 1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('home.html', posts=posts)
+    return render_template('home.html')
 
 @main.route("/blog")
 def blog():
